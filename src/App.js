10e2118c75fae2fdd59d698cfab6893a8fa5163c
RuslanBegishev <ruslanbegishev93@gmail.com>
2023-03-12 17:59:05 +0300

    Add todo tutorial

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import TodoList from './Todo/TodoList';
+import Context from './context';
+import AddTodo from './addTodo';
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'buy bread'},
+    {id: 2, completed: true, title: 'buy milk'},
+    {id: 3, completed: false, title: 'buy water'}
+  ])
+  
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+  return (
+    <Context.Provider value = {{removeTodo}}>
+      <div className='wrapper'>
+      <h1>React todo</h1>
+      <AddTodo onCreate={addTodo}/>
+      {todos.length ? <TodoList todos={todos} onToggle = {toggleTodo}/> : <p>No todos</p> }
+      
+
+     </div>
+    </Context.Provider>
+    
+  );
+}
+
+export default App;
